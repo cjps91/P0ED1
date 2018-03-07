721e004321f2d30c8cdb75c2932ec77704f78dbd
iCarlosPro <carlosjavier.perez@alu.uhu.es>
2018-03-07 19:28:29 +0100

    Primer commit

---
+++ P0ED1/p0.cpp
@@ -0,0 +1,13 @@
+#include "p0.h"
+#include <iostream>
+using namespace std;
+
+#include <conio.h>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXIMO 20
+#define M 5
+
---
+++ P0ED1/p0.h
@@ -0,0 +1,6 @@
+#ifndef P0_H_INCLUDED
+#define P0_H_INCLUDED
+
+
+
+#endif // P0_H_INCLUDED
