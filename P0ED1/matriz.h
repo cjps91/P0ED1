8b215ed4c5a8c0843eaadc10f40e3eedab16a2b5
iCarlosPro <carlosjavier.perez@alu.uhu.es>
2018-03-07 20:35:11 +0100

    Código añadido

--- P0ED1/matriz.h
+++ P0ED1/matriz.h
@@ -1,6 +1,26 @@
 #ifndef MATRIZ_H_INCLUDED
 #define MATRIZ_H_INCLUDED
 
+#include "quebrado.h"
+#include "complejo.h"
 
+#define M 5
+typedef quebrado tipoelemento;
+class matriz {
+tipoelemento celda[M][M];
+int fila,col;
+public:
+matriz(int f=0, int c=0);
+int getfila() { return fila; }
+int getcol() { return col; }
+bool operator==(matriz m);
+matriz operator*(matriz m);
+matriz operator+(matriz m);
+matriz operator-(matriz m);
+matriz operator-();
+void mostrar();
+void cargar(); //carga una matriz desde teclado
+void cargar(char cad[]);//carga una matriz desde fichero
+};
 
 #endif // MATRIZ_H_INCLUDED
--- P0ED1/quebrado.h
+++ P0ED1/quebrado.h
@@ -1,6 +1,24 @@
 #ifndef QUEBRADO_H_INCLUDED
 #define QUEBRADO_H_INCLUDED
 
-
+class quebrado {
+int numerador, denominador;
+public:
+quebrado() { numerador=0; denominador=1; }
+quebrado(int n, int d=1);
+int getnumerador() { return numerador; }
+int getdenominador() {return denominador; }
+void valor(int n, int d=1);
+quebrado operator+(quebrado q);
+quebrado operator-(quebrado q);
+quebrado operator*(quebrado q);
+quebrado operator/(quebrado q);
+quebrado operator-();
+int operator==(quebrado q);
+void mostrar();
+};
+// prototipo de funciones genéricas usadas en los métodos
+int mcd(int a, int b);
+int mcm(int a, int b);
 
 #endif // QUEBRADO_H_INCLUDED
